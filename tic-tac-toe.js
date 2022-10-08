77a1f306d6c5b0823046be7acf64b4685386cd70
Brittany Walker <walker.a.brittany@gmail.com>
2022-10-08 14:09:39 -0500

    added colors , x and o

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,10 +1,26 @@
 onload = () => {
     let board = document.getElementById("board");
-
+    let moveNum = 0;
+    let state = ['', '', '',
+                 '', '', '',
+                 '', '', ''];
     let x = 0;
-    console.log(board);
+
     while(x < board.children.length) {
-        board.children[x].className = 'square';
+        let square = board.children[x];
+        square.className = 'square';
+        square.addEventListener('click', function(){
+            if(moveNum % 2 == 0){
+                square.innerHTML = "X";
+                square.className += " X";
+            }
+            else{
+                square.innerHTML = "O";
+                square.className += " O";
+            }
+            moveNum = moveNum + 1;
+        });
         x++;
-    }  
+    } 
 }
+
